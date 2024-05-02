b259383dc1058a4c059b1bc88bdebe5daadea4a5
David_Bueno <david03bueno@gmail.com>
2024-05-02 11:04:04 -0400

    Este es un convertidor basico de grados Fr a Cl

---
+++ ConversionFr_Cl/src/main/java/asignacion/conversionfr_cl/ConversionFr_Cl.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package asignacion.conversionfr_cl;
+
+/**
+ *
+ * @author david
+ */
+public class ConversionFr_Cl {
+
+    public static void main(String[] args) {
+    
+    }
+}
---
+++ ConversionFr_Cl/src/main/java/asignacion/conversionfr_cl/Fahre_Celsius.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package asignacion.conversionfr_cl;
+
+/**
+ *
+ * @author david
+ * @matricula 2023-0243
+ */
+public class Fahre_Celsius {
+    
+    public double conversion(double fahrenheit){
+        double celsius = (fahrenheit - 32) * 0.5556;
+        return celsius;
+    }
+    
+}
